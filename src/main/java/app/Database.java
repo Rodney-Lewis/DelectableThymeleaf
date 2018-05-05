fd1a2c5871c43d97f3d4c833b34f648744291657
Rodney Lewis <rodneyll@umich.edu>
2018-05-05 01:52:13 -0400

    Database structure changes
    Added methods to close connections before the GC
    Added some form of error handling to methods
    Split the original query method into Query and Update

--- src/main/java/app/Database.java
+++ src/main/java/app/Database.java
@@ -6,31 +6,94 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import com.sun.media.jfxmedia.logging.Logger;
+
 public class Database {
 
-	static Connection GetConnection() throws SQLException {
+	private static Connection GetConnection() throws SQLException {
 		//TODO connection info should be at configuration level
-		try (Connection conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306", "root", "root")) {
-			return conn;
+		Connection conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306", "root", "root");
+		return conn;
+	}
+	
+	private static void CloseConnection(Connection conn) {
+		try {
+			if(conn != null)
+				conn.close();
+		}
+		catch (SQLException ex) {
+			ex.printStackTrace();
 		}
 	}
 	
-	static ResultSet Query(String sqlQuery) throws SQLException {
-		Connection queryConnection = GetConnection();
-		Statement queryStatement = queryConnection.createStatement();
-		ResultSet queryResults = queryStatement.executeQuery(sqlQuery);
+	private static void CloseStatement(Statement stmt) {
+		try {
+			if(stmt != null)
+				stmt.close();
+		}
+		catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	private static void CloseResultSet(ResultSet rs) {
+		try {
+			if(rs != null)
+				rs.close();
+		}
+		catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	public static ResultSet Query(String sqlQuery) {
+		Connection queryConnection = null;
+		Statement queryStatement = null;
+		ResultSet queryResults = null;
+		
+		try {
+			queryConnection = GetConnection();
+			queryStatement = queryConnection.createStatement();
+			queryResults = queryStatement.executeQuery(sqlQuery);
+			return queryResults;
+		}
+		catch (SQLException ex) {
+			//TODO Add rollback on exception and more useful logging with stacktrace
+			ex.printStackTrace();
+		}
+		finally {
+			CloseConnection(queryConnection);
+			CloseStatement(queryStatement);
+			CloseResultSet(queryResults);
+		}
 		return queryResults;
 	}
 	
-	static void CloseConnection(Connection conn) throws SQLException {
-		conn.commit();
-		conn.close();
+	public static String Update(String sqlQuery) {
+		int effectedRows = 0;
+		Connection updateConnection = null;
+		Statement updateStatement = null;
+		
+		try {
+			updateConnection = GetConnection();
+			updateStatement = updateConnection.createStatement();
+			effectedRows = updateStatement.executeUpdate(sqlQuery);
+			
+			if(effectedRows > 0) {
+				return StatusCode.DATABASE_UPDATE_OKAY.ToString();
+			}
+			else {
+				return StatusCode.DATABASE_NO_EFFECTED_ROWS.ToString();
+			}
+		}
+		catch (SQLException ex) {
+			//TODO Add rollback on exception and more useful logging with stacktrace
+			ex.printStackTrace();
+		}
+		finally {
+			CloseConnection(updateConnection);
+			CloseStatement(updateStatement);
+		}
+		return StatusCode.DATABASE_UPDATE_ERROR.ToString();
 	}
-	
-	static String Update(String sqlQuery) throws SQLException {
-		//TODO return statuses should exist in their own class/enum
-		String returnStatus = null;
-		return returnStatus;
-	}
-	
 }
