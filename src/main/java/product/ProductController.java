f371d9f76e7a8f437725573281c6268d9eb1d783
Rodney <rodneyll@umich.edu>
2018-09-12 20:55:55 -0400

    Updated ProductController and template
    Included nutrients to be displayed on page

--- src/main/java/product/ProductController.java
+++ src/main/java/product/ProductController.java
@@ -1,6 +1,8 @@
 package product;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
 
 import javax.sql.rowset.CachedRowSet;
 import org.springframework.stereotype.Controller;
@@ -15,14 +17,24 @@ public class ProductController {
 
     @GetMapping("/product/{id}")
     public String getProduct(Model model, @PathVariable int id) {
-    	String sql = "Select * from products where id=" + id;
-    	
+    	String sql = "";
+    	Collection<Nutrient> nutrients = new ArrayList<Nutrient>();
+
 		try {
+	    	sql = "Select * from products where id=" + id;
 			CachedRowSet set = Database.Query(sql);
 			set.last();
 			model.addAttribute("product", new Product(set.getInt("id"), set.getString("name"), set.getString("manufacturer"),
 					set.getFloat("servingSize"), set.getString("unitOfMeasure"), set.getInt("calories"), 
 					set.getString("ingredients"), set.getBoolean("ingredient"), set.getBoolean("premade")));
+			
+	    	sql = "Select * from nutrients where id=" + id;
+	    	set = Database.Query(sql);
+	    	
+	    	while(set.next()) {
+		    	nutrients.add(new Nutrient(set.getInt("id"), set.getString("name"), set.getFloat("value"), set.getString("unitOfMeasure")));
+	    	}
+			model.addAttribute("nutrients", nutrients);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
