592f33b184624d44270e3d7e9113011547f41b37
Rodney <rodneyll@umich.edu>
2018-09-22 08:01:44 -0400

    Moved fraction classes to a new package
    New package util will now hold helper classes not specific any class

---
+++ src/main/java/util/FractionHelper.java
@@ -0,0 +1,37 @@
+package util;
+
+public class FractionHelper {
+
+	static String ConvertFloatToFrac(float value) {
+		
+		int numberOfDecimalPlaces = 0;
+		int multiplicationFactor;
+		int numerator;
+		int GreatestCommonDenominator;
+		int denominator;
+		String fraction = "";
+		
+		numberOfDecimalPlaces = Float.toString(value).indexOf('.') -  Float.toString(value).length() - 1;
+		multiplicationFactor = (int) Math.pow(10, numberOfDecimalPlaces);
+		numerator = (int) (value * multiplicationFactor);
+		
+		GreatestCommonDenominator = FindGreatestCommonDenominator(numerator, multiplicationFactor);
+		
+		numerator = numerator / GreatestCommonDenominator;
+		denominator = multiplicationFactor / GreatestCommonDenominator;
+		
+		fraction = Integer.toString(numerator) + '/' + Integer.toString(denominator);
+		return fraction;
+	}
+	
+	static int FindGreatestCommonDenominator(int a, int b) {
+		//Euclidean division
+		int temp;
+		while(b != 0) {
+			temp = b;
+			b = a % b;
+			a = temp;
+		}
+		return a;
+	}
+}
