568d91286c357e9813ac1351bae38d8fe1903a6d
Rodney <rodneyll@umich.edu>
2018-09-24 21:16:17 -0400

    Updated controller for the form and existing product page
    Controller has methods to handle generating the form and adding/removing of nutrients from the page. Added form.html test these.
    
    Also updated controller to handle the nutrients list from products rather then as a seperate model entity. This change is reflected in product.html.

--- src/main/java/product/ProductController.java
+++ src/main/java/product/ProductController.java
@@ -4,12 +4,17 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.sql.rowset.CachedRowSet;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import app.Database;
 
 @Controller
@@ -18,27 +23,52 @@ public class ProductController {
     @GetMapping("/product/{id}")
     public String getProduct(Model model, @PathVariable int id) {
     	String sql = "";
-    	Collection<Nutrient> nutrients = new ArrayList<Nutrient>();
 
 		try {
 	    	sql = "Select * from products where id=" + id;
 			CachedRowSet set = Database.Query(sql);
 			set.last();
-			model.addAttribute("product", new Product(set.getInt("id"), set.getString("name"), set.getString("manufacturer"),
-					set.getFloat("servingSize"), set.getString("unitOfMeasure"), set.getInt("calories"), 
-					set.getString("ingredients"), set.getBoolean("ingredient"), set.getBoolean("premade")));
 			
+			Product prod = new Product(set.getInt("id"), set.getString("name"), set.getString("manufacturer"),
+					set.getFloat("servingSize"), set.getString("unitOfMeasure"), set.getInt("calories"), 
+					set.getString("ingredients"), set.getBoolean("ingredient"), set.getBoolean("premade"));
+						
 	    	sql = "Select * from nutrients where id=" + id;
 	    	set = Database.Query(sql);
 	    	
 	    	while(set.next()) {
-		    	nutrients.add(new Nutrient(set.getInt("id"), set.getString("name"), set.getFloat("value"), set.getString("unitOfMeasure")));
+		    	prod.nutrients.add(new Nutrient(set.getInt("id"), set.getString("name"), set.getFloat("value"), set.getString("unitOfMeasure")));
 	    	}
-			model.addAttribute("nutrients", nutrients);
+			model.addAttribute("product", prod);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
     	return "product.html";
+    }
+    
+    @GetMapping("/product")
+    public String greetingForm(Model model) {
+        model.addAttribute("product", new Product());
+        return "form";
+    }
+
+    @PostMapping(value="/product", params= {"save"})
+    public String greetingSubmit(@ModelAttribute Product product) {
+        return "result";
+    }
+    
+    @RequestMapping(value="/product", params={"addRow"})
+    public String addRow(final Product product) {
+        product.getNutrients().add(new Nutrient());
+        return "form";
+    }
+    
+    @RequestMapping(value="/product", params={"removeRow"})
+    public String removeRow(final Product product, final HttpServletRequest req) {
+        final Integer rowId = Integer.valueOf(req.getParameter("removeRow"));
+        product.getNutrients().remove(rowId.intValue());
+        product.getNutrients().trimToSize();
+        return "form";
     }
 }
 
