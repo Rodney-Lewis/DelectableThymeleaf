4d531b989025e13063b3256c99fcf28c697178ac
Rodney-Lewis <rodneyll@umich.edu>
2018-10-08 18:13:50 -0400

    SQL changes
    Updated sql statements to work with jbdc 2.3.0
    Properly closing cachedrowsets
    Disabled the nutrients queries

--- src/main/java/product/ProductController.java
+++ src/main/java/product/ProductController.java
@@ -2,18 +2,15 @@ package product;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Collection;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.sql.rowset.CachedRowSet;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import app.Database;
 
@@ -23,25 +20,33 @@ public class ProductController {
     @GetMapping("/product/{id}")
     public String getProduct(Model model, @PathVariable int id) {
     	String sql = "";
-
+    	CachedRowSet set = null;
+    	
 		try {
-	    	sql = "Select * from products where id=" + id;
-			CachedRowSet set = Database.Query(sql);
+	    	sql = "Select * FROM `products` WHERE id=" + id;
+			set = Database.Query(sql);
 			set.last();
 			
 			Product prod = new Product(set.getInt("id"), set.getString("name"), set.getString("manufacturer"),
 					set.getFloat("servingSize"), set.getString("unitOfMeasure"), set.getInt("calories"), 
 					set.getString("ingredients"), set.getBoolean("ingredient"), set.getBoolean("premade"));
-						
-	    	sql = "Select * from nutrients where id=" + id;
-	    	set = Database.Query(sql);
+					
+//			Holding off on Nutrient until a proper format is determined.
+//	    	sql = "Select * FROM `nutrients` WHERE id=" + id;
+//	    	set = Database.Query(sql);
 	    	
-	    	while(set.next()) {
-		    	prod.nutrients.add(new Nutrient(set.getInt("id"), set.getString("name"), set.getFloat("value"), set.getString("unitOfMeasure")));
-	    	}
+//	    	while(set.next()) {
+//		    	prod.nutrients.add(new Nutrient(set.getInt("id"), set.getString("name"), set.getFloat("value"), set.getString("unitOfMeasure")));
+//	    	}
 			model.addAttribute("product", prod);
 		} catch (SQLException e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				set.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
     	return "product";
     }
@@ -49,10 +54,11 @@ public class ProductController {
     @GetMapping("/product/list")
     public String getProductList(Model model) {
     	String sql = "";
+    	CachedRowSet set = null;
 
 		try {
 	    	sql = "Select * from products";
-			CachedRowSet set = Database.Query(sql);
+			set = Database.Query(sql);
 			
 			ArrayList<Product> productList = new ArrayList<Product>();
 						
@@ -64,6 +70,12 @@ public class ProductController {
 			model.addAttribute("productList", productList);
 		} catch (SQLException e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				set.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
     	return "productList";
     }
