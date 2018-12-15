17f56f2b0ff6a63629e04bbafcc883751813c9e1
Rodney <rodneyll@umich.edu>
2018-12-15 08:18:14 -0500

    Determined UI structure for consistency
    Updated nav and header fragments to work based on variables
    Removed naming inconsistencies

--- src/main/java/delectable/app/controller/IndexController.java
+++ src/main/java/delectable/app/controller/IndexController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class PageController {
+public class IndexController {
 
 	@GetMapping("")
 	public String goHome() {
-		return "general/index";
+		return "index";
 	}
 }
