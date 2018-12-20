7f719ee1263711c09c8e389d6afc6767b2b0e21a
Rodney-Lewis <rodneyll@umich.edu>
2018-12-20 00:33:34 -0500

    Added dummy add and edit buttons

--- src/main/java/delectable/app/controller/VendorController.java
+++ src/main/java/delectable/app/controller/VendorController.java
@@ -5,8 +5,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import delectable.app.entity.Vendor;
 import delectable.app.service.VendorService;
 
 @Controller
@@ -21,4 +24,24 @@ public class VendorController {
 		return "vendor/list";
 	}
 
+	@GetMapping("/vendor")
+	public String addVendor(Model model) {
+		model.addAttribute("vendor", new Vendor());
+		return "vendor/form";
+	}
+
+	@GetMapping("/vendor/{id}")
+	public String getVendor(Model model, @PathVariable int id) {
+		return "vendor/form";
+	}
+
+	@GetMapping("/vendor/{id}/edit")
+	public String editVendor(Model model, @PathVariable int id) {
+		return "vendor/form";
+	}
+
+	@DeleteMapping("/vendor/{id}/remove")
+	public void deleteVendor() {
+
+	}
 }
